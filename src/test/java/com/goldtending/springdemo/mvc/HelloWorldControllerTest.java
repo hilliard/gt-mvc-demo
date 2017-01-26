c86f8e18393d7b27660efbf0bf9ec0a68d20c272
hscott <hilliards@gmail.com>
2017-01-26 07:21:55 -0700

    Added data and controller mods

---
+++ src/test/java/com/goldtending/springdemo/mvc/HelloWorldControllerTest.java
@@ -0,0 +1,14 @@
+package com.goldtending.springdemo.mvc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldControllerTest {
+
+	@Test
+	public void test() {
+		fail ("Not yet implemented");
+	}
+
+}
